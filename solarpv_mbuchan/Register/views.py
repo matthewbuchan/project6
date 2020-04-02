ad6a19a33f732b9351d6347d1f667ddd97a6b8ef
Matthew Buchan <mbuchan7@asu.edu>
2020-04-02 07:01:34 +0000

    app Register removed, app solarpv added

--- solarpv_mbuchan/Register/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-# Create your views here.
-def Register(request):
-    return HttpResponse("<H1>Hi there REGISTRATION</H1>")
