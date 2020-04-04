68e1f183d74dfc64649f4d6399e19eaae9470ddf
Matthew Buchan <mbuchan7@asu.edu>
2020-04-04 23:59:00 +0000

    models/relationships configured

--- solarpv_mbuchan/solarpv_mbuchan/settings.py
+++ solarpv_mbuchan/solarpv_mbuchan/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'solarpv_mbuchan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'solarpv',
+        'HOST' : 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : 'password',
+        'PORT' : '3306'
     }
 }
 
